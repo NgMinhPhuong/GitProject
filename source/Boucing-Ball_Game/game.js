fd02d65f2d62204bf5be8be951f2298aadfebc30
Nguyễn Minh Phương <nguyenminhphuong813@gmail.com>
2023-10-18 11:42:22 +0700

    Thực hành bouncing Ball Game

--- source/Boucing-Ball_Game/game.js
+++ source/Boucing-Ball_Game/game.js
@@ -1,4 +1,4 @@
-const SPEED = 10;       
+const SPEED = 1;       
 
 class GameBoard{
     constructor(width, height){
@@ -8,7 +8,7 @@ class GameBoard{
         a.style.width = this.width+'px';
         a.style.height = this.height+'px';
     }
-}
+};
 
 class Ball{
     constructor(x, y, size, speedMove){
@@ -16,8 +16,6 @@ class Ball{
         this.y = y;
         this.speedMove = speedMove;
         this.size = size;
-        this.xcu = x;
-        this.ycu = y;
         this.thechua = document.createElement('div')
         let a = document.querySelector('.board')
         this.thechua.classList.add('ball');
@@ -26,62 +24,68 @@ class Ball{
         this.thechua.style.width = this.size + 'px';
         this.thechua.style.height = this.size + 'px';
         a.append(this.thechua);
-        this.direction = 1;
+        this.direction = Math.ceil(Math.random() * 4);
     }
 
     wayMove(){
+        if(this.y >= game.height - this.size) 
+        {
+            cnt--;
+            this.thechua.remove();this.x = 0; this.y = 0;
+            this.direction = 0;
+        }
+        else{
         if(this.direction == 1)
-    {
-        if(this.y <= 0) this.direction = 2;
-        else if(this.x >= game.width - this.size) this.direction = 4;
-        else {
-            this.x += this.speedMove;
-            this.y -= this.speedMove;
-            this.moveBall();
-           
+        {
+            if(this.y <= 0) this.direction = 2;
+            else if(this.x >= game.width - this.size) this.direction = 4;
+            else {
+                this.x += this.speedMove;
+                this.y -= this.speedMove;
+                this.moveBall();
+            }
         }
-    }
-    else if(this.direction == 2)
-    {
-        if(this.x >= game.width - this.size) this.direction = 3;
-        else if(this.y >= bar.top - this.size) this.direction = 1;
-        else {
-            this.x += this.speedMove;
+        else if(this.direction == 2)
+        {
+            if(this.x >= game.width - this.size) this.direction = 3;
+            else if(this.y >= bar.top - this.size && this.y <= bar.top
+                && this.x >= (bar.x - this.size) && this.x <= (bar.x + bar.width)) this.direction = 1;
+            else {
+                this.x += this.speedMove;
             this.y += this.speedMove;
-            this.moveBall();
-            
+                this.moveBall();  
+            }
         }
-    }
-    else if(this.direction == 3)
-    {
-        if(this.y >= bar.top - this.size) this.direction = 4;
-        else if(this.x <= 0) this.direction = 2;
-        else {
-            this.x -= this.speedMove;
-            this.y += this.speedMove;
-            this.moveBall();
-            
+        else if(this.direction == 3)
+        {
+            if(this.y >= bar.top - this.size && this.y <= bar.top
+                && this.x >= (bar.x - this.size) && this.x <= (bar.x + bar.width)) this.direction = 4;
+            else if(this.x <= 0) this.direction = 2;
+            else {
+                this.x -= this.speedMove;
+                this.y += this.speedMove;
+                this.moveBall();
+            }
         }
-    }
-    else if(this.direction == 4)
-    {
-        if(this.x <= 0) this.direction = 1;
-        else if(this.y <= 0) this.direction = 3;
-        else {
-            this.x -= this.speedMove;
-            this.y -= this.speedMove;
-            this.moveBall();
+        else if(this.direction == 4)
+        {
+            if(this.x <= 0) this.direction = 1;
+            else if(this.y <= 0) this.direction = 3;
+            else {
+                this.x -= this.speedMove;
+                this.y -= this.speedMove;
+                this.moveBall();
            
+            }
         }
-    }
+        }
     }
 
     moveBall(){
-        
         this.thechua.style.left = this.x + 'px';
         this.thechua.style.top = this.y  + 'px';
     }
-}
+};
 
 class Bar{
     constructor(width, height, x, speed, top){
@@ -112,18 +116,7 @@ class Bar{
         a.style.left = this.x + 'px'
         console.log(this.x, this.width);
     }
-}
-    
-let game = new GameBoard(400, 400);
-let bar = new Bar(80, 12, 0, 10, 320)
-let ball = [];
-for(let i = 0; i < 100; i++)
-{
-    let x = Math.random()*(game.width - SPEED);
-    let y = Math.random()*(bar.top - SPEED);
-    ball[i] = new Ball(x, y, SPEED, 1);
-}
-
+};
 window.onkeydown = function(e){
    
     switch (e.which) {
@@ -135,15 +128,33 @@ window.onkeydown = function(e){
             break;
     }
 }
-
-
-let MoveBall = setInterval(function()
-{ 
-    for(let i = 0; i < 100; i++)
-    {
-        ball[i].wayMove() 
-    }    
-},0)
-
+var cnt = 233;
+var game = new GameBoard(400, 400);
+var bar = new Bar(80, 12, 0, 10, 320)
+var ball = [];
+for(let i = 0; i < 233; i++)
+{
+    let x = Math.random()*(game.width - SPEED);
+    let y = Math.random()*(bar.top - SPEED);
+    ball[i] = new Ball(x, y, 10, SPEED);
+}
+var col = ['gray','green','red','blue','white','pink','black','yellow']
+var co = 0;
+function play(){
+    let MoveBall = setInterval(function()
+    {             
+        for(let i = 0; i < 233; i++)
+        {
+            ball[i].wayMove();
+            ball[i].thechua.style.backgroundColor = col[co];
+            co++;
+            if(co == col.length) co = 0; 
+        }
+        if(cnt == 0) {
+            document.querySelector('.board').innerHTML = '<h1>Thua rồi đồ Ngu Ngục!</h1>';
+            clearInterval(MoveBall); 
+        }  
+    },20)
+}
 
 
